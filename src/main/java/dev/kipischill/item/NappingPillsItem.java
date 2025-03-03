111cdc1debc7bfe5edecd15653b22a9f91fb91fc
kipischill <rowansalmon9@gmail.com>
2025-03-02 20:41:18 -0400

    Removed Trinkets API not needed

---
+++ src/main/java/dev/kipischill/item/NappingPillsItem.java
@@ -0,0 +1,9 @@
+package dev.kipischill.item;
+
+import net.minecraft.item.Item;
+
+public class NappingPillsItem extends Item {
+    public NappingPillsItem(Settings settings) {
+        super(settings);
+    }
+}
