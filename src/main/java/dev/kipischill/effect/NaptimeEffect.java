d6498d0880eb05e0934a807be905d8f44175fb3b
kipischill <rowansalmon9@gmail.com>
2025-03-03 15:37:32 -0400

    Naptime

---
+++ src/main/java/dev/kipischill/effect/NaptimeEffect.java
@@ -0,0 +1,10 @@
+package dev.kipischill.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class NaptimeEffect extends StatusEffect {
+    public NaptimeEffect(StatusEffectCategory category, int color) {
+        super(category, color);
+    }
+}
---
+++ src/main/java/dev/kipischill/registry/ModEffects.java
@@ -0,0 +1,20 @@
+package dev.kipischill.registry;
+
+import dev.kipischill.UnnamedDelight;
+import dev.kipischill.effect.NaptimeEffect;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModEffects {
+    public static StatusEffect registerStatusEffect(String name) {
+        return Registry.register(Registries.STATUS_EFFECT, new Identifier(UnnamedDelight.MOD_ID, name),
+                new NaptimeEffect(StatusEffectCategory.NEUTRAL, 0x63a1ff));
+    }
+
+    public static void registerEffects() {
+        UnnamedDelight.LOGGER.info("Registering Mod Effects for " + UnnamedDelight.MOD_ID);
+    }
+}
