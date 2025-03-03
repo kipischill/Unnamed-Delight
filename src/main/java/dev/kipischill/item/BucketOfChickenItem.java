2b869e92d4f3ad39ed4a99582a8448cf9bc27ac9
kipischill <rowansalmon9@gmail.com>
2025-03-03 08:15:39 -0400

    Fixed Items and added Napping pills

--- src/main/java/dev/kipischill/item/BucketOfChickenItem.java
+++ src/main/java/dev/kipischill/item/BucketOfChickenItem.java
@@ -4,7 +4,6 @@ import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -18,7 +17,7 @@ public class BucketOfChickenItem extends Item {
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(Text.translatable("tooltip.bucket_of_chicken.tooltip").formatted(Formatting.GRAY));
+        tooltip.add(Text.translatable("tooltip.bucket_of_chicken.tooltip"));
         super.appendTooltip(stack, world, tooltip, context);
     }
 }
--- src/main/java/dev/kipischill/item/NappingPillsItem.java
+++ src/main/java/dev/kipischill/item/NappingPillsItem.java
@@ -1,9 +1,22 @@
 package dev.kipischill.item;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class NappingPillsItem extends Item {
     public NappingPillsItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.napping_pills.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
