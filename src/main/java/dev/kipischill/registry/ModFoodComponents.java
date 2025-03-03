09d4da8874ae2bb25fac7b3e12eee65bd1d919d1
kipischill <rowansalmon9@gmail.com>
2025-03-03 08:15:54 -0400

    Added Napping Pills Component

--- src/main/java/dev/kipischill/registry/ModFoodComponents.java
+++ src/main/java/dev/kipischill/registry/ModFoodComponents.java
@@ -10,4 +10,9 @@ public class ModFoodComponents {
             .statusEffect(new StatusEffectInstance(ModEffects.NOURISHMENT.get(), 600, 0), 1f)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 2), 1f)
             .alwaysEdible().build();
+
+    public static final FoodComponent NAPPING_PILLS = new FoodComponent.Builder().hunger(0).saturationModifier(0f)
+            .statusEffect(new StatusEffectInstance(ModEffects.NOURISHMENT.get(), 600, 0), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 2), 1f)
+            .alwaysEdible().build();
 }
