f4f6f30d8463c8da64fa3438d965bbaeed6ed940
kipischill <rowansalmon9@gmail.com>
2025-03-03 16:09:33 -0400

    Naptime v2

--- src/main/java/dev/kipischill/UnnamedDelight.java
+++ src/main/java/dev/kipischill/UnnamedDelight.java
@@ -1,9 +1,14 @@
 package dev.kipischill;
 
+import dev.kipischill.effect.NaptimeEffect;
+import dev.kipischill.registry.ModEffects;
 import dev.kipischill.registry.ModItemGroups;
 import dev.kipischill.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +16,19 @@ public class UnnamedDelight implements ModInitializer {
 	public static final String MOD_ID = "unnameddelight";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static boolean isComatose(@Nullable LivingEntity entity) {
+		return entity != null &&
+				entity.hasStatusEffect(ModEffects.NAPTIME) &&
+				!entity.isSpectator() &&
+				!(entity instanceof PlayerEntity player && player.isCreative());
+	}
+
 	@Override
 	public void onInitialize() {
+		ModEffects.registerEffects();
 		ModItems.registerModItems();
 
 		ModItemGroups.registerItemGroups();
+
 	}
 }
--- src/main/java/dev/kipischill/registry/ModEffects.java
+++ src/main/java/dev/kipischill/registry/ModEffects.java
@@ -9,12 +9,16 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModEffects {
-    public static StatusEffect registerStatusEffect(String name) {
+
+    public static StatusEffect NAPTIME;
+
+    public static StatusEffect registerNaptimeEffect(String name) {
         return Registry.register(Registries.STATUS_EFFECT, new Identifier(UnnamedDelight.MOD_ID, name),
                 new NaptimeEffect(StatusEffectCategory.NEUTRAL, 0x63a1ff));
     }
 
     public static void registerEffects() {
+        NAPTIME = registerNaptimeEffect("naptime");
         UnnamedDelight.LOGGER.info("Registering Mod Effects for " + UnnamedDelight.MOD_ID);
     }
 }
