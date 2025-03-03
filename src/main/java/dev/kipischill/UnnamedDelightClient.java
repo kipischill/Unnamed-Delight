bee8fd2fa55ec22841d4cfcd97dc66ee8f80f0d7
kipischill <rowansalmon9@gmail.com>
2025-03-03 17:06:29 -0400

    Added Satin API and black screen when naptime

--- src/main/java/dev/kipischill/UnnamedDelightClient.java
+++ src/main/java/dev/kipischill/UnnamedDelightClient.java
@@ -1,10 +1,23 @@
 package dev.kipischill;
 
+import ladysnake.satin.api.event.ShaderEffectRenderCallback;
+import ladysnake.satin.api.managed.ManagedShaderEffect;
+import ladysnake.satin.api.managed.ShaderEffectManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
 
 public class UnnamedDelightClient implements ClientModInitializer {
+    private static final ManagedShaderEffect BLACK_SCREEN = ShaderEffectManager.getInstance()
+            .manage(new Identifier("unnameddelight", "shaders/post/blackscreen.json"));
+
     @Override
     public void onInitializeClient() {
 
+        ShaderEffectRenderCallback.EVENT.register(tickDelta -> {
+            if (UnnamedDelight.isComatose(MinecraftClient.getInstance().player)) {
+                BLACK_SCREEN.render(tickDelta);
+            }
+        });
     }
 }
